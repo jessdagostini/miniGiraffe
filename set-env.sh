dbbd64891013a3f46f8af2ab4bbfc6d80a4f3b85
Jessica Dagostini <jessicadagostini@gmail.com>
2024-09-12 20:43:24 -0700

    Bash to set environment

---
+++ set-env.sh
@@ -0,0 +1,1 @@
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/soe/jessicadagostini/miniGiraffe/deps/gbwt/lib:/soe/jessicadagostini/miniGiraffe/deps/libhandlegraph/lib/usr/local/lib:/soe/jessicadagostini/miniGiraffe/
